21a499f780fc3d6117cc561ad102a0483f3b6cd4
Enrique <enrique.bermudez@ucr.ac.cr>
2025-04-17 17:50:50 -0600

    warning make (separar header y cpp)

---
+++ tarea_oper/Resources.cpp
@@ -0,0 +1,46 @@
+#include "Resources.hpp"
+
+void sort_array(std::vector<int>& values) {
+  int min = 0;
+  int index = 0;
+  int pivot = 0;
+  for (size_t i = 0; i < values.size(); i++) {
+    index = i;
+    min = values[i];
+    for (size_t j = i + 1; j < values.size(); j++) {
+      if (values[j] < min) {
+        min = values[j];
+        index = j;
+      }
+    }
+    pivot = values[i];
+    values[i] = values[index];
+    values[index] = pivot;
+  }
+}
+
+void sort_mayor_menor(std::vector<int>& values) {
+  int max = 0;
+  int index = 0;
+  int pivot = 0;
+  for (size_t i = 0; i < values.size(); i++) {
+    max = values[i];
+    index = i;
+    for (size_t j = i + 1; j < values.size(); j++) {
+      if (values[j] > max) {
+        max = values[j];
+        index = j;
+      }
+    }
+    pivot = values[i];
+    values[i] = max;
+    values[index] = pivot;
+  }
+}
+
+void init_data(data_t data[], std::vector<int>& values) {
+  for (size_t i = 0; i < values.size(); i++) {
+    data[i].value = values[i];
+    data[i].attended = false;
+  }
+}
--- tarea_oper/Resources.hpp
+++ tarea_oper/Resources.hpp
@@ -1,51 +1,15 @@
+#ifndef RESOURCES_HPP
+#define RESOURCES_HPP
+
 #include <vector>
 
-typedef struct{
+typedef struct {
   int value;
   bool attended;
-}data_t;
+} data_t;
 
-static void sort_array(std::vector<int>& values) {
-  int min = 0;
-  int index = 0;
-  int pivot = 0;
-  for(size_t i = 0; i < values.size(); i++) {
-    index = i;
-    min = values[i];
-    for(size_t j = i+1; j < values.size(); j++) {
-      if(values[j] < min) {
-        min = values[j];
-        index = j;
-      }
-    }
-    pivot = values[i];
-    values[i] = values[index];
-    values[index] = pivot;
-  }
-}
+void sort_array(std::vector<int>& values);
+void sort_mayor_menor(std::vector<int>& values);
+void init_data(data_t data[], std::vector<int>& values);
 
-static void sort_mayor_menor(std::vector<int>& values) {
-  int max = 0;
-  int index = 0;
-  int pivot = 0;
-  for(size_t i = 0; i < values.size(); i++) {
-    max = values[i];
-    index = i;
-    for(size_t j = i+1; j < values.size(); j++) {
-      if(values[j] > max) {
-        max = values[j];
-        index = j;
-      }
-    }
-    pivot = values[i];
-    values[i] = max;
-    values[index] = pivot;
-  }
-}
-
-static void init_data(data_t data[], std::vector<int>& values) {
-  for(size_t i = 0; i < values.size(); i++) {
-    data[i].value = values[i];
-    data[i].attended = false;
-  }
-}
+#endif
